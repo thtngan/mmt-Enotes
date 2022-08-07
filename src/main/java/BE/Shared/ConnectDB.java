bbc09772b195f0e8c6262d1a09393db379ee337d
thtngan <ththaongan1303@gmail.com>
2022-08-07 19:59:23 +0700

    Final submit

--- src/main/java/BE/Shared/ConnectDB.java
+++ src/main/java/BE/Shared/ConnectDB.java
@@ -18,10 +18,10 @@ public class ConnectDB {
       conn = DriverManager.getConnection(dbURL, user, pass);
       if (conn != null) {
         DatabaseMetaData dm = conn.getMetaData();
-                System.out.println("Driver name: " + dm.getDriverName());
-                System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: " + dm.getDatabaseProductName());
-                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+//                System.out.println("Driver name: " + dm.getDriverName());
+//                System.out.println("Driver version: " + dm.getDriverVersion());
+//                System.out.println("Product name: " + dm.getDatabaseProductName());
+//                System.out.println("Product version: " + dm.getDatabaseProductVersion());
       }
     } catch (SQLException ex) {
       System.out.println(ex.getMessage());
